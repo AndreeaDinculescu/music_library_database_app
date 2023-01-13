d619ac94e1a1db7f09880f74c2992b5c87edc65e
AndreeaDinculescu <andreeadinculescu90@gmail.com>
2023-01-13 10:32:21 +0000

    first commit

---
+++ post_artists_diagram.md
@@ -0,0 +1,14 @@
+```mermaid
+sequenceDiagram
+  participant C as Client
+  participant A as Sinatra Application Class (app.rb)
+  participant R as Repository Class ArtistRepository
+  participant D as Database Music Library
+  Note left of C: Flow of time <br />⬇ <br /> ⬇ <br /> ⬇ 
+  C->>A: HTTP Request method is POST, address is http://localhost:9292, path is /artists, parameters are name and genre. Forwards request POST /artists to the Sinatra Application(app.rb)
+  A->>R: Calls method post /artists on body params name=Wild nothing,genre=Indie. Calls method create(new_artist) in ArtistRepository class
+  R->>D: in ArtistRepository class in method created(new_artist) INSERT INTO artists (name, genre)  VALUES (Wild Nothing, Indie)
+  D->>R: Returns the new Artist object
+  R->>A: ArtistRepository Class returns an Artist to the Sinatra Application Class(app.rb)
+  A->>C: HTTP Response: the route block returns response (e.g. ‘200 0k’) to the client.
+```
---
+++ spec/integration/application_spec.rb
@@ -0,0 +1,89 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  before(:each) do
+    reset_albums_table
+    reset_artists_table
+  end
+
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  context 'GET /albums' do
+    it 'should return the list of albums' do
+      response = get('/albums')
+
+      # <br />
+  
+      expect(response.status).to eq(200)
+      
+      expect(response.body).to include('<a href="/albums/2">Surfer Rosa</a><br />')
+      expect(response.body).to include('<a href="/albums/3">Waterloo</a><br />')
+      expect(response.body).to include('<a href="/albums/4">Super Trouper</a><br />')
+      expect(response.body).to include('<a href="/albums/5">Bossanova</a><br />')
+      end
+    end
+
+  context 'GET /albums/id' do
+    it 'should return info about album 1' do
+      response = get ('/albums/1')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include('<h1>Doolittle</h1>')
+      expect(response.body).to include('Release year: 1989')
+      expect(response.body).to include('Artist: Pixies')
+    end
+  end
+
+
+    context 'POST /albums' do
+      it 'should create a new album' do
+        response = post(
+          '/albums',
+          title: 'Voyage',
+          release_year: '2002',
+          artist_id: '2'
+          )
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq('')
+
+        response = get('/albums')
+
+        expect(response.body).to include('Voyage')
+      end
+    end
+
+    context 'GET /artists' do
+      it 'should return the list of artists' do
+        response = get('/artists')
+
+        expected_response = 'Pixies, ABBA, Taylor Swift, Nina Simone'
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(expected_response)
+      end
+    end
+
+    context 'POST /artists' do
+      it 'should create a new artist' do
+        response = post(
+          '/artists',
+          name: 'Wild nothing',
+          genre: 'Indie'
+          )
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq('')
+
+        response = get('/artists')
+
+        expect(response.body).to include('Wild nothing')
+      end
+    end
+end
